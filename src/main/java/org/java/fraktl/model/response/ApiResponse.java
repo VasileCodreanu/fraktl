2217d0f60ac5f220b5fe321eae794e774fcb6896
Vasile Codreanu <codreanu.vasile.gh@gmail.com>
2025-08-13 23:10:04 +0300

    change ApiResponse properties naming

--- src/main/java/org/java/fraktl/model/response/ApiResponse.java
+++ src/main/java/org/java/fraktl/model/response/ApiResponse.java
@@ -2,10 +2,12 @@ package org.java.fraktl.model.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
-public class ApiResponse {
-    private String message;
-    private Object response;
+public class ApiResponse<T> {
+    private String status;
+    private T data;
 }
