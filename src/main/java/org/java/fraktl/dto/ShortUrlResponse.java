d518b0468e88e3181aca9852703575b22cf513f5
Vasile Codreanu <codreanu.vasile.gh@gmail.com>
2025-12-07 13:35:55 +0200

    refactor ShortUrlResponse

--- src/main/java/org/java/fraktl/dto/ShortUrlResponse.java
+++ src/main/java/org/java/fraktl/dto/ShortUrlResponse.java
@@ -1,24 +1,31 @@
 package org.java.fraktl.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
+import lombok.Builder;
 import org.java.fraktl.entity.ShortenedUrl;
 
+@Builder
 public record ShortUrlResponse(
     String shortCode,
     String shortUrl,
     String originalUrl,
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     LocalDateTime createdAt,
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     LocalDateTime expiresAt
 ) {
 
   public static ShortUrlResponse from(ShortenedUrl entity) {
-    return new ShortUrlResponse(
-        entity.getShortCode(),
-        entity.getShortUrl(),
-        entity.getOriginalUrl(),
-        entity.getCreatedAt(),
-        entity.getExpiresAt()
-    );
+    return ShortUrlResponse.builder()
+        .shortCode(entity.getShortCode())
+        .shortUrl(entity.getShortCode())
+        .originalUrl(entity.getOriginalUrl())
+        .createdAt(entity.getCreatedAt())
+        .expiresAt(entity.getExpiresAt())
+        .build();
   }
 
 }
